62f782ccbf3967348d223ed1b06d96f1fcf15154
Viet H Nguyen <viet.nguyen182@gmail.com>
2014-04-05 08:28:59 -0400

    Refactor

--- find_duplicate.py
+++ find_duplicate.py
@@ -1,10 +1,17 @@
+
+
 '''
-find duplicate files on a specific directory
+HOW TO RUN 
+- Run as program:
+	python3 find_duplicate.py [folder name]
+
+Find duplicate files on a specific directory
 TO DO: 
 	- check file size first		OK
 	- sort file size		OK	
 	- check files that have the same size 	OK
-	- calculate MD5			
+	- check MD5 of the first few bytes 
+	- calculate MD5				OK
 	- finally check file content with regard to bytes		
 '''
 
@@ -29,13 +36,15 @@ def find_dupplicate(root):
 	refsize = 0
 	fnlist = []
 	for line in open(summarysorted):
-		token = line.split() # must be sure each line has two tokens
+		token = line.split() 
 		size = int(token[0])
 		if size > refsize :	# 
-			# do md5 comparing on this set 
-			md5_dup_list = detect_md5_dup(fnlist)
-			for g in md5_dup_list:
-				res.append(g)
+			# do md5 comparing on this list 
+			md5_dup_list_coarse = detect_md5_dup_coarse(fnlist)
+			for g in md5_dup_list_coarse:
+				md5_dup_list = detect_md5_dup(g)
+				for g2 in  md5_dup_list:
+					res.append(g2)
 			del fnlist[:]
 			refsize = size
 		
@@ -43,7 +52,36 @@ def find_dupplicate(root):
 		fnlist.append(filename)
 	return res
 
-# Given a list of file names, export names of the files that have the same md5s
+# Given a list of file names, export names of the files that have the same md5s - COURSE-GRAINED
+def detect_md5_dup_coarse(fnlist):
+	res = []
+	md5dict = {}
+	for fn in fnlist:
+		md5 = calc_md5_coarse(fn)
+		if md5 not in md5dict:
+			md5dict[md5] = [fn]
+		else:
+			md5dict[md5].append(fn)
+	for k in md5dict.keys():
+		if len(md5dict[k]) >=2:
+			res.append(md5dict[k])
+	return res
+	
+def calc_md5_coarse(filename):
+	if os.path.isfile(filename) == False:
+	 	return 0
+	res = ''
+	try:
+		with open(filename, mode = 'rb') as f:
+			d = hashlib.md5()
+			buf = f.read(128)
+			d.update(buf)
+			res = d.hexdigest()
+	except PermissionError:
+		pass
+	return res	
+
+# Given a list of file names, export names of the files that have the same md5s - FIND-GRAINED
 def detect_md5_dup(fnlist):
 	res = []
 	md5dict = {}
@@ -52,7 +90,6 @@ def detect_md5_dup(fnlist):
 		if md5 not in md5dict:
 			md5dict[md5] = [fn]
 		else:
-			print("Duplicate MD5!")
 			md5dict[md5].append(fn)
 	for k in md5dict.keys():
 		if len(md5dict[k]) >=2:
@@ -65,11 +102,14 @@ def calc_md5(filename):
 	if os.path.isfile(filename) == False:
 	 	return 0
 	res = ''
-	with open(filename, mode = 'rb') as f:
-		d = hashlib.md5()
-		for buf in iter(partial(f.read, 128), b''):
-			d.update(buf)
-		res = d.hexdigest()
+	try:
+		with open(filename, mode = 'rb') as f:
+			d = hashlib.md5()
+			for buf in iter(partial(f.read, 128), b''):
+				d.update(buf)
+			res = d.hexdigest()
+	except PermissionError:
+		pass
 	return res
 	
 	
@@ -85,6 +125,7 @@ def list_files(root, outfilename):
 				filesize = info[stat.ST_SIZE];
 				summary.write(str(filesize) + "\t" + path   + "\n");
 		
+# sort a summary file, based on file size
 def sort_size(inname, outname):
 	# sort according to file size 
 	sizedict = {}
